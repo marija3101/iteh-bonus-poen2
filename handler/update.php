1a443ee262f8666ba91701720922541aabed6898
Marija Martinovic <marijaamartinovicc11@gmail.com>
2021-10-28 21:26:48 +0200

    Ažurirana UPDATE funkcija u update.php

--- handler/update.php
+++ handler/update.php
@@ -1,5 +1,22 @@
 <?php
 
-// kreirati  uodate funkciju
+require "../dbBroker.php";
+require "../model/prijava.php";
 
-?> 
+if(isset($_POST['id']) && isset($_POST['predmet']) && isset($_POST['katedra']) && isset($_POST['sala']) && isset($_POST['datum'])) { 
+
+    $obj = new Prijava($_POST['id'] ,$_POST['predmet'], $_POST['katedra'], $_POST['sala'], $_POST['datum']);
+    $status = $obj->update($_POST['id'], $conn);
+    
+    if($status) {
+        echo 'Success'; 
+    }
+    else {
+        echo 'Failed';
+    }
+}
+
+?>
+
+
+
